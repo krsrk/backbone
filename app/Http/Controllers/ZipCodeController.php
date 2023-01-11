f16ae33da763de5e6bdf22d040c280de6add030d
Chris López <christian.lopez.be@gmail.com>
2023-01-11 09:02:51 -0600

    fix(): record not found fail on resource

--- app/Http/Controllers/ZipCodeController.php
+++ app/Http/Controllers/ZipCodeController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ZipCodeRequest;
 use App\Http\Resources\ZipCodeResource;
 use App\Repositories\ZipCodeRepository;
+use Illuminate\Http\Response;
 
 class ZipCodeController extends Controller
 {
     public function __construct(protected ZipCodeRepository $repository){}
 
-    public function index(ZipCodeRequest $request, string $zipCode)
+    public function index(ZipCodeRequest $request, Response $response, string $zipCode)
     {
-        return response()->json(
-            new ZipCodeResource($this->repository->findBy('zip_code', $zipCode))
-        );
+        $zipCodes = $this->repository->findBy('zip_code', $zipCode);
+        $responseData = (is_null($zipCodes)) ? [] : new ZipCodeResource($zipCodes);
+        $responseCode = (is_null($zipCodes)) ?  $response::HTTP_NOT_FOUND : $response::HTTP_OK;
+
+        return response()->json($responseData, $responseCode);
     }
 }
